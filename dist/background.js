ad06a169703827a4d855e173aad50fe45c9c37f3
Onkar Dighe <onkardigheofficial@gmail.com>
2026-01-23 11:03:56 +0530

    Stop tracking dist folder

--- dist/background.js
+++
@@ -1,6 +0,0 @@
-// src/background.ts
-chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true }).catch((error) => console.error(error));
-chrome.runtime.onInstalled.addListener(() => {
-  console.log("Echo extension installed");
-});
-//# sourceMappingURL=background.js.map
